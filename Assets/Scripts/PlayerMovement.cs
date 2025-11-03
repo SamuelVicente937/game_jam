da9800d83219a78dd9940d697289b85ab1129282
samuel <vicentesamue937@gmail.com>
2025-11-03 00:13:30 -0400

    mecanicas_base31125

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,39 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 5f;
     private Rigidbody2D rb;
+    public Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()   
+    void Update()
     {
         float move = Input.GetAxis("Horizontal");
+
+        // mover horizontal
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
 
+        // salto
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        // --- elegir animación ---
+        if (Mathf.Abs(move) > 0.1f)
+        {
+            animator.Play("run");
+        }
+        else
+        {
+            animator.Play("idle");
+        }
+
+        if (move > 0.1f) // derecha
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (move < -0.1f) // izquierda
+            transform.localScale = new Vector3(-1, 1, 1);
     }
 }
